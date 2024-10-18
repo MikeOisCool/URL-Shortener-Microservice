53158eca24c8755192c29d922727d122fc131222
Michael Ouazzani <morphois77@web.de>
2024-10-18 10:50:47 +0200

    feat: finish

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -15,8 +16,39 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let urlDatabase = [];
+app.use(bodyParser.urlencoded({ extended: false }));
+const isValidUrl = (url) => {
+  const urlPattern = /^(http|https):\/\/[^ "]+$/;
+  return urlPattern.test(url);
+};
+
+
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  
+  if (!isValidUrl(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Generiere eine ID (short_url)
+  const shortUrl = urlDatabase.length + 1;
+  urlDatabase.push({ original_url: originalUrl, short_url: shortUrl });
+
+  res.json({ original_url: originalUrl, short_url: shortUrl });
+});
+
+// GET: Weiterleitung von der Kurz-URL zur Original-URL
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = parseInt(req.params.short_url);
+  const urlEntry = urlDatabase.find(entry => entry.short_url === shortUrl);
+
+  if (urlEntry) {
+    return res.redirect(urlEntry.original_url);
+  } else {
+    return res.json({ error: "No URL found for this short_url" });
+  }
 });
 
 app.listen(port, function() {
